d2ea45fb4f4cae174784bb8c5efac3f8c7eb3539
Gabriela Ordonez <00361516@uca.edu.sv>
2019-04-05 10:28:21 -0600

    Initial commit

---
+++ app/src/main/java/com/gordonez/laboratorio4/MovieAdapter.kt
@@ -0,0 +1,50 @@
+package com.gordonez.laboratorio4
+
+import android.support.v7.view.menu.ActionMenuItemView
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.cardview_movie.view.*
+
+
+class MovieAdapter (var movies: List<Movie>): RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MovieAdapter.ViewHolder {
+        val view = LayoutInflater.from(p0.context).inflate(R.layout.cardview_movie, p0, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+
+    override fun onBindViewHolder(holder: MovieAdapter.ViewHolder, pos: Int) = holder.bind(movies[pos])
+
+    fun changeList(movies: List<Movie>){
+        this.movies = movies
+        notifyDataSetChanged()
+    }
+
+
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind (item: Movie) = with(itemView){
+
+            Glide.with(itemView.context)
+                .load(item.Poster)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(movie_image_cv)
+
+            movie_title_cv.text = item.Title
+            movie_plot_cv.text = item.Plot
+            movie_rate_cv.text = item.imbdRating
+            movie_runtime_cv.text = item.RunTime
+
+        }
+    }
+
+
+
+}
